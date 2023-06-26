eb35567ce4d40e8868214d2cf099af147a6bd1fa
Samyak Haribhakta <110559769+Sivernial@users.noreply.github.com>
2023-06-26 11:35:41 +0530

    Google Auth 2.0 Done!

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import './App.css';
-import Footer from './assets/footer/Footer';
-import SplashPage from './assets/splashpage/SplashPage';
-import Registration from './components/registration/Registration';
-import Signup from './components/signup_signin/Signup';
+import "./App.css";
+import Footer from "./assets/footer/Footer";
+import SplashPage from "./assets/splashpage/SplashPage";
+import Registration from "./components/registration/Registration";
+import Signup from "./components/signup_signin/Signup";
 
 function App() {
   return (
     <>
-      {/* <SplashPage/> */}
-      {/* <Signup/> */}
-      <Registration/>
+      {/* <SplashPage /> */}
+      <Signup />
+      {/* <Registration/> */}
       {/* <Footer/> */}
     </>
   );
--- src/components/signup_signin/Signup.js
+++ src/components/signup_signin/Signup.js
@@ -1,36 +1,38 @@
-import React from 'react'
-import './signup.css'
+import React from "react";
+import "./signup.css";
+import { GoogleOAuthProvider, GoogleLogin } from "@react-oauth/google";
 
 const Signup = () => {
   return (
     <>
-        <div className='signupcontainer'>
-            <div className='signupheading'>  
-                <h2>Welcome to</h2>
-                <h2 className='signupiitjeats'>IITJEATS</h2>
-                <span> All your all favourite eating spots under one roof.</span>
-                <span className='signupiitjeats'> Order Now</span>
-            </div>
-
-
-            <div className='signupbottom'>
-                <div className='signinwithtext'>
-                    <span>Signin With</span> 
-                </div>
-
-                <a href='/' className='signupbutton'>
-                    <div>
-                          <img className='signupgoogleiconbox' src="https://www.freepnglogos.com/uploads/google-logo-png/google-logo-png-suite-everything-you-need-know-about-google-newest-0.png" alt="" />
-                    </div>
-
-                    <span className='siginingoogletext'>GOOGLE</span>
-
-                </a>
-            </div>
-
+      <div className="signupcontainer">
+        <div className="signupheading">
+          <h2>Welcome to</h2>
+          <h2 className="signupiitjeats">IITJEATS</h2>
+          <span> All your all favourite eating spots under one roof.</span>
+          <span className="signupiitjeats"> Order Now</span>
         </div>
-    </>
-  )
-}
 
-export default Signup
+        <div className="signupbottom">
+          <div className="signinwithtext">
+            <span>Signin With</span>
+          </div>
+          <GoogleOAuthProvider clientId="503531894137-qn0rmeumsi509sqm848oaddd5ikgq5l6.apps.googleusercontent.com">
+            <GoogleLogin
+              onSuccess={(credentialResponse) => {
+                console.log(credentialResponse);
+              }}
+              onError={() => {
+                console.log("Login Failed");
+              }}
+              useOneTap
+            />
+          </GoogleOAuthProvider>
+          ;
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Signup;
