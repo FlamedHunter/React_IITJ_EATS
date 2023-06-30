b3476c59feefa3044c8914ed1f3133a7295834d9
Arman Garg <armangarg36@gamil.com>
2023-06-30 20:56:11 +0530

    Added back button

--- src/App.js
+++ src/App.js
@@ -5,12 +5,15 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route exact path="/" element={<HomePage/>} />
-        <Route exact path="/profile" element={<Profile/>} />
-      </Routes>
-    </Router>
+    <>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<HomePage/>} />
+          <Route exact path="/profile" element={<Profile/>} />      
+        </Routes>
+      </Router>
+    </>
+
   );
 }
 export default App;
--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -36,6 +36,19 @@ const Home = () => {
 
         </div>
 
+
+        <div className='homefooter'>
+            <h1>
+               Eat  Learn  Repeat
+            </h1>
+            <span>
+              <span className='homefootertext'>Created with</span>
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="rgba(203, 0, 0, 0.649)" class="bi bi-heart-fill" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>
+              </svg>
+              <span className='homefootertext2'>by students only</span>
+            </span>
+        </div>
     </>
   )
 }
